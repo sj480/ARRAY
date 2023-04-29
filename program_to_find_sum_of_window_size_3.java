0adc6fe14d85e8649eb63499f43d2826cfaf60c1
swapnil shirish janrao <73775658+sj480@users.noreply.github.com>
2023-04-30 01:16:36 +0530

    Update program_to_find_sum_of_window_size_3.java

--- program_to_find_sum_of_window_size_3.java
+++ program_to_find_sum_of_window_size_3.java
@@ -1,20 +1,14 @@
-#include <iostream>
-using namespace std;
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 1, 4, 3, 9, 7, 6, 8};
+        int n = array.length;
 
-int main() {
-    int array[9] = {5, 2, 1, 4, 3, 9, 7, 6, 8};
-
-    // Find the size of the array
-    int n = sizeof(array)/sizeof(array[0]);
-
-    // Loop through the array to calculate the sum of all windows of size 3
-    for (int i = 0; i <= n-3; i++) {
-        int sum = 0;
-        for (int j = i; j < i+3; j++) {
-            sum += array[j];
+        for (int i = 0; i <= n-3; i++) {
+            int sum = 0;
+            for (int j = i; j < i+3; j++) {
+                sum += array[j];
+            }
+            System.out.println("Sum of window starting at index " + i + " is " + sum);
         }
-        cout << "Sum of window starting at index " << i << " is " << sum << endl;
     }
-
-    return 0;
 }
